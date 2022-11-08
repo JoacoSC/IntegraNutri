5a6a04dea63c90f89d826a9088bafd562283a973
Joaquin Salinas <salinascastillojoaquin@gmail.com>
2022-11-08 13:00:08 -0300

    scrollbar estilizados, agregado boton Close en el modal

--- src/ui/ModalPacienteEspontaneo.jsx
+++ src/ui/ModalPacienteEspontaneo.jsx
@@ -35,6 +35,11 @@ export const ModalPacienteEspontaneo = () => {
                 ariaHideApp={false}
                 className="modal-container"
                 >
+                <div className="btn-modal-close" onClick={ () => setOpenModal(false) }>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="#FFFFFF" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M18 6 6 18M6 6l12 12"/>
+                    </svg>
+                </div>
                 <h1 className="modal-header">
                     Agregar paciente espontáneo
                 </h1>
