ba876f46b88d008dddeacec53ab34ecb6f837112
Joaquin Salinas <salinascastillojoaquin@gmail.com>
2022-11-10 11:00:22 -0300

    agregado el formulario para editar agenda, enviando datos a la consola

--- src/ui/ModalEditJournal.jsx
+++ src/ui/ModalEditJournal.jsx
@@ -10,11 +10,17 @@ export const ModalEditJournal = () => {
 
     const [openModal, setOpenModal] = useState(false);
 
-    const { name, onInputChange } = useForm();
+    const { workingDayHours, workingDayMinutes, consultationHours, consultationMinutes, consultationAmount, onInputChange } = useForm({
+        workingDayHours: '08',
+        workingDayMinutes: '00',
+        consultationHours: '00',
+        consultationMinutes: '15',
+        consultationAmount: '8',
+    });
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        console.log({ name });
+        console.log({ workingDayHours, workingDayMinutes, consultationHours, consultationMinutes, consultationAmount });
     }
 
     return (
@@ -42,13 +48,115 @@ export const ModalEditJournal = () => {
                 <form onSubmit={ onSubmit }>
                     <div className="container-form" onSubmit={ onSubmit }>
 
-                        <div className="form-item">
-                            <label className="input-label"></label>
-                            <input className="input-text-style" type="text" name="name" onChange={ onInputChange }/>
+                        <div className="form-group">
+                            <label className="edit-journal-label">Hora de inicio de la jornada:</label>
+                            <div className="form-item w-50 pr-8">
+                                <label className="input-label">Hora</label>
+                                <select className="select-style" defaultValue={ workingDayHours } name="workingDayHours" onChange={ onInputChange }>
+                                    <option value="00">00</option>
+                                    <option value="01">01</option>
+                                    <option value="02">02</option>
+                                    <option value="03">03</option>
+                                    <option value="04">04</option>
+                                    <option value="05">05</option>
+                                    <option value="06">06</option>
+                                    <option value="07">07</option>
+                                    <option value="08">08</option>
+                                    <option value="09">09</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    
+                                </select>
+                            </div>
+                            <div className="form-item w-50 pl-8">
+                            <label className="input-label">Minutos</label>
+                                <select className="select-style" placeholder="00" name="workingDayMinutes" onChange={ onInputChange }>
+                                    <option value="00">00</option>
+                                    <option value="05">05</option>
+                                    <option value="10">10</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                    <option value="25">25</option>
+                                    <option value="30">30</option>
+                                    <option value="35">35</option>
+                                    <option value="40">40</option>
+                                    <option value="45">45</option>
+                                    <option value="50">50</option>
+                                    <option value="55">55</option>
+                                </select>
+                            </div>      
+                        </div>
+                        <div className="form-group">
+                            <label className="edit-journal-label">Tiempo de cada consulta:</label>
+                            <div className="form-item w-50 pr-8">
+                                <label className="input-label">Horas</label>
+                                <select className="select-style" name="consultationHours" onChange={ onInputChange }>
+                                    <option value="0">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                            <div className="form-item w-50 pl-8">
+                                <label className="input-label">Minutos</label>
+                                <select className="select-style" name="consultationMinutes" onChange={ onInputChange }>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                    <option value="25">25</option>
+                                    <option value="30">30</option>
+                                    <option value="35">35</option>
+                                    <option value="40">40</option>
+                                    <option value="45">45</option>
+                                    <option value="50">50</option>
+                                    <option value="55">55</option>
+                                </select>
+                            </div>      
+                        </div>
+                        <div className="form-group">
+                            <label className="edit-journal-label">Cantidad de consultas en la jornada:</label>
+                            <div className="form-item w-50 pr-8">
+                                <label className="input-label">Consultas</label>
+                                <select className="select-style" defaultValue={ consultationAmount } name="consultationAmount" onChange={ onInputChange }>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    
+                                </select>
+                            </div>
+                            <div className="form-item w-50 pl-8"></div> 
                         </div>
                         <div className="form-btn">
                             <button className="btn-modal-submit" type="submit" onClick={ () => setOpenModal(false) }>
-                                Registrarse
+                                Aplicar
                             </button>
                         </div>
                     </div>
