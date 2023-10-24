04540724c5c41feb0348234c681524b6826f3079
Joaquin Salinas Castillo <salinascastillojoaquin@gmail.com>
2023-10-24 11:25:48 -0300

    modificando modalEditJournal, checkboxes funcionando

--- src/ui/ModalEditJournal.jsx
+++ src/ui/ModalEditJournal.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from 'react-modal';
 import { useDispatch, useSelector } from "react-redux";
 import { CSSTransition } from "react-transition-group";
@@ -12,27 +12,118 @@ export const ModalEditJournal = () => {
 
     const [openModal, setOpenModal] = useState(false);
 
+    const [isMondayChecked, setIsMondayChecked] = useState(false);
+    const [isTuesdayChecked, setIsTuesdayChecked] = useState(false);
+    const [isWednesdayChecked, setIsWednesdayChecked] = useState(false);
+    const [isThursdayChecked, setIsThursdayChecked] = useState(false);
+    const [isFridayChecked, setIsFridayChecked] = useState(false);
+    const [isSaturdayChecked, setIsSaturdayChecked] = useState(false);
+    const [isSundayChecked, setIsSundayChecked] = useState(false);
+
     const dispatch = useDispatch()
 
     const {
         journalID,
     } = useSelector((state) => state.journal);
 
-    const { workingDayStartHours, workingDayStartMinutes, consultationHours, consultationMinutes, consultationsPerDay, onInputChange, formState } = useForm({
-        workingDayStartHours: 8,
-        workingDayStartMinutes: 0,
-        consultationHours: 0,
-        consultationMinutes: 0,
-        consultationsPerDay: 8,
-    });
+    const {
+        mondayStartTime,
+        mondayEndTime,
+        tuesdayStartTime,
+        tuesdayEndTime,
+        wednesdayStartTime,
+        wednesdayEndTime,
+        thursdayStartTime,
+        thursdayEndTime,
+        fridayStartTime,
+        fridayEndTime,
+        saturdayStartTime,
+        saturdayEndTime,
+        sundayStartTime,
+        sundayEndTime,
+        onInputChange,
+        formState } = useForm();
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        console.log({ workingDayStartHours, workingDayStartMinutes, consultationHours, consultationMinutes, consultationsPerDay });
-        dispatch( startEditJournal( journalID, workingDayStartHours, workingDayStartMinutes, consultationHours, consultationMinutes, consultationsPerDay ) );
+        
+        if( isMondayChecked ){
+            console.log({mondayStartTime, mondayEndTime})
+        }
+        if( isTuesdayChecked ){
+            console.log({tuesdayStartTime, tuesdayEndTime})
+        }
+        if( isWednesdayChecked ){
+            console.log({wednesdayStartTime, wednesdayEndTime})
+        }
+        if( isThursdayChecked ){
+            console.log({thursdayStartTime, thursdayEndTime})
+        }
+        if( isFridayChecked ){
+            console.log({fridayStartTime, fridayEndTime})
+        }
+        if( isSaturdayChecked ){
+            console.log({saturdayStartTime, saturdayEndTime})
+        }
+        if( isSundayChecked ){
+            console.log({sundayStartTime, sundayEndTime})
+        }
+
+        console.log(isMondayChecked)
+        console.log(isTuesdayChecked)
+        console.log(isWednesdayChecked)
+        console.log(isThursdayChecked)
+        console.log(isFridayChecked)
+        console.log(isSaturdayChecked)
+        console.log(isSundayChecked)
+        
+        // dispatch( startEditJournal( journalID, workingDayStartHours, workingDayStartMinutes, consultationHours, consultationMinutes, consultationsPerDay ) );
         
         
     }
+
+    const handleMondayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsMondayChecked( current => !current )
+    }
+
+    const handleTuesdayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsTuesdayChecked( current => !current )
+    }
+
+    const handleWednesdayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsWednesdayChecked( current => !current )
+    }
+
+    const handleThursdayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsThursdayChecked( current => !current )
+    }
+
+    const handleFridayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsFridayChecked( current => !current )
+    }
+
+    const handleSaturdayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsSaturdayChecked( current => !current )
+    }
+
+    const handleSundayCheckbox = ( event ) => {
+        // const isChecked = event.target.checked;
+
+        setIsSundayChecked( current => !current )
+    }
+
 
     const onModalOpen = () => {
 
@@ -45,6 +136,16 @@ export const ModalEditJournal = () => {
         setOpenModal(false)
         dispatch( isEditingJournal( false ))
     }
+
+    useEffect(() => {
+        setIsMondayChecked( false )
+        setIsTuesdayChecked( false )
+        setIsWednesdayChecked( false )
+        setIsThursdayChecked( false )
+        setIsFridayChecked( false )
+        setIsSaturdayChecked( false )
+        setIsSundayChecked( false )
+    }, [openModal])
 
     return (
         <>
@@ -69,7 +170,145 @@ export const ModalEditJournal = () => {
                 </h1>
 
                 <form onSubmit={ onSubmit }>
-                    <div className="container-form" onSubmit={ onSubmit }>
+                    <div className="modal-edit-journal-container-form" onSubmit={ onSubmit }>
+                    <div className="flex-column">
+                        <label className="pt-1 pb-2">Días de la semana</label>
+                        <div className="journal-row-container">
+                            <div className="journal-row-item">
+                                <input type="checkbox" name="checkEverything"></input> 
+                                <label className="">Seleccionar todos</label>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isMondayChecked } onChange={ handleMondayCheckbox }/>                                
+                                <label className="">Lunes:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="mondayStartTime" disabled={ !isMondayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="mondayEndTime" disabled={ !isMondayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isTuesdayChecked } onChange={ handleTuesdayCheckbox }/> 
+                                <label className="">Martes:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="tuesdayStartTime" disabled={ !isTuesdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="tuesdayEndTime" disabled={ !isTuesdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isWednesdayChecked } onChange={ handleWednesdayCheckbox }/> 
+                                <label className="">Miércoles:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="wednesdayStartTime" disabled={ !isWednesdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="wednesdayEndTime" disabled={ !isWednesdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isThursdayChecked } onChange={ handleThursdayCheckbox }/> 
+                                <label className="">Jueves:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="thursdayStartTime" disabled={ !isThursdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="thursdayEndTime" disabled={ !isThursdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isFridayChecked } onChange={ handleFridayCheckbox }/> 
+                                <label className="">Viernes:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="fridayStartTime" disabled={ !isFridayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="fridayEndTime" disabled={ !isFridayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isSaturdayChecked } onChange={ handleSaturdayCheckbox }/> 
+                                <label className="">Sábado:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="saturdayStartTime" disabled={ !isSaturdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="saturdayEndTime" disabled={ !isSaturdayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="journal-row-container">
+                            <div className="journal-row-title">
+                                <input type="checkbox" value={ isSundayChecked } onChange={ handleSundayCheckbox }/> 
+                                <label className="">Domingo:</label>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Inicio de la jornada:</label>
+                                <input type="time" name="sundayStartTime" disabled={ !isSundayChecked } onChange={ onInputChange }></input>
+                            </div>
+                            <div className="journal-row-item">
+                                <label className="">Fin de la jornada:</label>
+                                <input type="time" name="sundayEndTime" disabled={ !isSundayChecked } onChange={ onInputChange }></input>
+                            </div>
+                        </div>
+                            {/* <div className="form-item w-50 pr-8">
+                                <label className="input-label">Hora</label>
+                                <select className="select-style" defaultValue={ workingDayStartHours } name="workingDayStartHours" onChange={ onInputChange }>
+                                    <option value="0">00</option>
+                                    <option value="1">01</option>
+                                    <option value="2">02</option>
+                                    
+                                </select>
+                            </div>
+                            <div className="form-item w-50 pl-8">
+                            <label className="input-label">Minutos</label>
+                                <select className="select-style" placeholder="00" name="workingDayStartMinutes" onChange={ onInputChange }>
+                                    <option value="0">00</option>
+                                    <option value="5">05</option>
+                                    <option value="10">10</option>
+                                </select>
+                            </div>       */}
+                        </div>
+                        <div className="form-btn">
+                            <button className="btn-modal-submit" type="submit" onClick={ () => setOpenModal(false) }>
+                                Aplicar
+                            </button>
+                        </div>
+                    </div>
+                    {/* <div className="container-form" onSubmit={ onSubmit }>
 
                         <div className="form-group">
                             <label className="edit-journal-label">Hora de inicio de la jornada:</label>
@@ -182,7 +421,7 @@ export const ModalEditJournal = () => {
                                 Aplicar
                             </button>
                         </div>
-                    </div>
+                    </div> */}
                 </form>
                 </Modal>
             </CSSTransition>
